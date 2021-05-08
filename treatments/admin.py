b4091abbb6470182da032a9ba7405c7d0bc1aa12
mxhaib <jack200034@hotmail.com>
2021-05-08 19:00:42 +0100

    Customised admin for treatments/categories

--- treatments/admin.py
+++ treatments/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Treatment, Category
 
 
-admin.site.register(Treatment)
-admin.site.register(Category)
+class TreatmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Treatment, TreatmentAdmin)
+admin.site.register(Category, CategoryAdmin)
