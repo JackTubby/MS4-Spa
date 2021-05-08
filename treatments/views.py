9a69ee011e4daf2ed083af64cae641b110d114c4
mxhaib <jack200034@hotmail.com>
2021-05-08 19:12:47 +0100

    Added treatment views and template

--- treatments/views.py
+++ treatments/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Treatment
 
-# Create your views here.
+
+from django.shortcuts import render
+
+
+def all_treatments(request):
+    """ A view to return all treatments, including sorting and search quries """
+
+    treatments = Treatment.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+
+    return render(request, 'treatments/treatments.html', context)
+
