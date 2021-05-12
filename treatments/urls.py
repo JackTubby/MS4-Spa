e0cf6bc8d6f48c85d92b1fb80b4550c7cc34bd63
mxhaib <jack200034@hotmail.com>
2021-05-12 16:35:42 +0100

    Add treatment detail view, style its page

--- treatments/urls.py
+++ treatments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_treatments, name='treatments')
+    path('', views.all_treatments, name='treatments'),
+    path('<treatment_id>', views.treatment_detail, name='treatment_detail'),
 ]
--- treatments/views.py
+++ treatments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Treatment
 
 
@@ -16,3 +16,14 @@ def all_treatments(request):
 
     return render(request, 'treatments/treatments.html', context)
 
+
+def treatment_detail(request, treatment_id):
+    """ A view to show individual treatment details """
+
+    treatments = get_object_or_404(Treatment, pk=treatment_id)
+
+    context = {
+        'treatments': treatments,
+    }
+
+    return render(request, 'treatments/treatment_detail.html', context)
