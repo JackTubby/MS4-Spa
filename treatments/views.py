6da71e5a5a6121bf8082ee3220b773728c554541
mxhaib <jack200034@hotmail.com>
2021-05-14 18:24:25 +0100

    Added search functionality

--- treatments/views.py
+++ treatments/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, reverse, render
+from django.contrib import messages
+from django.db.models import Q
 from .models import Treatment
 
 
@@ -9,9 +11,20 @@ def all_treatments(request):
     """ A view to return all treatments, including sorting and search quries """
 
     treatments = Treatment.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            treatments = treatments.filter(queries)
 
     context = {
         'treatments': treatments,
+        'search_term': query,
     }
 
     return render(request, 'treatments/treatments.html', context)
