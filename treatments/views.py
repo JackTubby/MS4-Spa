fe1a5dc3c06e3b5ecd901df5fed419a39eb49807
mxhaib <jack200034@hotmail.com>
2021-05-16 19:09:53 +0100

    Adding treatment sorting by price/rating

--- treatments/views.py
+++ treatments/views.py
@@ -13,9 +13,23 @@ def all_treatments(request):
     treatments = Treatment.objects.all()
     query = None
     categories = None
-
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']#
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                treatments = treatments.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            treatments = treatments.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             treatments = treatments.filter(category__name__in=categories)
@@ -29,10 +43,13 @@ def all_treatments(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             treatments = treatments.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'treatments': treatments,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting ': current_sorting,
     }
 
     return render(request, 'treatments/treatments.html', context)
