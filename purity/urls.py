d779d3d429f3415e9f589b353b5a8d761e278a11
mxhaib <jack200034@hotmail.com>
2021-05-26 13:59:09 +0100

    Add checkout url

--- purity/urls.py
+++ purity/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('treatments/', include('treatments.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
