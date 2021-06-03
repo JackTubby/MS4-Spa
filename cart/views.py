1abc91deafd084edfb822cecb1fd0887f0b800a6
JackTubby <jack200034@hotmail.com>
2021-06-03 17:42:56 +0000

    Add adjust cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
 
 def view_cart(request):
@@ -21,6 +22,25 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified treatment to the specified amount"""
+
+    treatment = get_object_or_404(Treatment, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request, f'Updated {treatment.name} quantity to {cart[item_id]}')
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed {treatment.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
 
 
 def remove_from_cart(request, item_id):
