21966412353f282e41be204179b79d4b87b129ae
JackTubby <jack200034@hotmail.com>
2021-06-04 19:13:06 +0000

    Add profiles path into url patterns

--- purity/urls.py
+++ purity/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('treatments/', include('treatments.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
