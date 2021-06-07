8549349602d39c1c40a585af6387924600979b5a
JackTubby <jack200034@hotmail.com>
2021-06-07 12:29:21 +0000

    Add save form functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -11,11 +12,18 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     orders = profile.orders.all()
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updates Successfully')
+
     form = UserProfileForm(instance=profile)
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
