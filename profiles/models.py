49187a020d4b97fa40406693102d6addf1cbf21e
JackTubby <jack200034@hotmail.com>
2021-06-07 10:49:21 +0000

    Fix mistake for users not going to the verify page

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django_countries.fields import CountryField
@@ -8,10 +8,10 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    A user profile model for maintaining default delivery information
-    and order history
+    A user profile model for maintaining default
+    delivery information and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -31,5 +31,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile
-        instance.userprofile.save()
+    # Existing users: just save the profile
+    instance.userprofile.save()
