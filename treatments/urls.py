d89b3b31cb75271cf044430c3a3f148a515a0194
JackTubby <jack200034@hotmail.com>
2021-06-08 19:05:26 +0000

    Add delete treatment path

--- treatments/urls.py
+++ treatments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:treatment_id>/', views.treatment_detail, name='treatment_detail'),
     path('add/', views.add_treatment, name='add_treatment'),
     path('edit/<int:treatment_id>/', views.edit_treatment, name='edit_treatment'),
+    path('delete/<int:treatment_id>/', views.delete_treatment, name='delete_treatment'),
 ]
