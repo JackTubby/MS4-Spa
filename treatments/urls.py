2a75a97f175c848c7b354f66d67709669d9dc5ea
JackTubby <jack200034@hotmail.com>
2021-06-08 18:48:10 +0000

    Add edit treatment path

--- treatments/urls.py
+++ treatments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_treatments, name='treatments'),
     path('<int:treatment_id>/', views.treatment_detail, name='treatment_detail'),
     path('add/', views.add_treatment, name='add_treatment'),
+    path('edit/<int:treatment_id>/', views.edit_treatment, name='edit_treatment'),
 ]
