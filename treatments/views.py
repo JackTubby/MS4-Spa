dea6dbf6c2e2bda8c0ae39efca3500f060c64e35
JackTubby <jack200034@hotmail.com>
2021-06-08 18:48:21 +0000

    Add edit treatment view

--- treatments/views.py
+++ treatments/views.py
@@ -41,10 +41,10 @@ def all_treatments(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('treatments'))
-    
+  
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             treatments = treatments.filter(queries)
- 
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -85,5 +85,27 @@ def add_treatment(request):
     context = {
         'form': form,
     }
+    return render(request, template, context)
 
+
+def edit_treatment(request, treatment_id):
+    """ Edit a treatment """
+    treatment = get_object_or_404(Treatment, pk=treatment_id)
+    if request.method == 'POST':
+        form = TreatmentForm(request.POST, request.FILES, instance=treatment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated treatment.')
+            return redirect(reverse('treatment_detail', args=[treatment.id]))
+        else:
+            messages.error(request, 'Failed to update treatment. Please make sure the form is valid!')
+    else:
+        form = TreatmentForm(instance=treatment)
+        messages.info(request, f'You are editing {treatment.name}')
+
+    template = 'treatments/edit_treatment.html'
+    context = {
+        'form': form,
+        'treatment': treatment,
+    }
     return render(request, template, context)
