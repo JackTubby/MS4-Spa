83538e2732e2f8cfd904fcf8022b7917f2b2d69a
JackTubby <jack200034@hotmail.com>
2021-06-10 21:57:03 +0000

    Add all_blogs view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    """ A view to return all treatments, including sorting and search quries """
+
+    blog = Blog.objects.all()
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog.html', context)
