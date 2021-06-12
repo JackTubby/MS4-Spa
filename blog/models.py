e786578046502930f08508f0a9f8c0eb4dda341b
JackTubby <jack200034@hotmail.com>
2021-06-12 19:28:30 +0000

    Add image fields in to blog model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Blog(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
