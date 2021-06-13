90180003b8bc367eb8521f3418eef37ca4556939
JackTubby <jack200034@hotmail.com>
2021-06-13 21:16:59 +0000

    Add blog details view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def all_blogs(request):
-    """ A view to return all treatments, including sorting and search quries """
+    """ A view to return all blogs """
 
     blog = Blog.objects.all()
 
@@ -12,3 +12,15 @@ def all_blogs(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, blog_id):
+    """ A view to show individual blog details """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
