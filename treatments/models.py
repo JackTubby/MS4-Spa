8813002e6eda617d7532d6b56aaeeac437ff0d4b
JackTubby <jack200034@hotmail.com>
2021-06-14 21:08:51 +0000

    Create rating model

--- treatments/models.py
+++ treatments/models.py
@@ -22,9 +22,15 @@ class Treatment(models.Model):
     description = models.TextField()
     amount = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
