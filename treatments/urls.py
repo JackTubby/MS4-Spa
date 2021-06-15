16e9b1f1a7d14e8f350ffba1b79eb3e3f5576b98
JackTubby <jack200034@hotmail.com>
2021-06-15 21:26:16 +0000

    Add treatment id into add_rating

--- treatments/urls.py
+++ treatments/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.all_treatments, name='treatments'),
     path('<int:treatment_id>/', views.treatment_detail, name='treatment_detail'),
     path('add/', views.add_treatment, name='add_treatment'),
-    path('add_rating/', views.add_rating, name='add_rating'),
+    path('add_rating/<int:treatment_id>/', views.add_rating, name='add_rating'),
     path('edit/<int:treatment_id>/', views.edit_treatment, name='edit_treatment'),
     path('delete/<int:treatment_id>/', views.delete_treatment, name='delete_treatment'),
 ]
