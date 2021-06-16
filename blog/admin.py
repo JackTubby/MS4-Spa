e134061d2f2c353414ba0678a8668fee154eaa85
JackTubby <jack200034@hotmail.com>
2021-06-16 22:13:13 +0000

    Add BlogAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Blog
 
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'image_url',
+        'image',
+    )
+
+
+admin.site.register(Blog, BlogAdmin)
