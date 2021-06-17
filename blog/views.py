252235fd0bc21fc97bf7a80406fbe058f0dc92db
JackTubby <jack200034@hotmail.com>
2021-06-17 19:23:28 +0000

    Add edit blog view

--- blog/views.py
+++ blog/views.py
@@ -51,3 +51,31 @@ def add_blog(request):
         'form': form,
     }
     return render(request, template, context)
+
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Edit a blog """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog.')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to update blog. Please make sure the form is valid!')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {blog.name}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+    return render(request, template, context)
