9ae59a465eea34d3fb854931470e3482a7935aca
JackTubby <jack200034@hotmail.com>
2021-06-20 14:14:54 +0000

    Add faq page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 
+# Home Index Page
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+# Faq Page
+def faq(request):
+    """ A view to return the faq page """
+
+    return render(request, 'home/faq.html')
