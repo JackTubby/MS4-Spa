cafb63ca9cb729a539edd3b9f106e4ca257753d0
JackTubby <jack200034@hotmail.com>
2021-06-23 11:16:05 +0000

    Add refund policy url

--- treatments/urls.py
+++ treatments/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit/<int:treatment_id>/', views.edit_treatment, name='edit_treatment'),
     path('delete/<int:treatment_id>/', views.delete_treatment, name='delete_treatment'),
     path('add_rating/<int:treatment_id>/', views.add_rating, name='add_rating'),
+    path('refund_policy/', views.refund_policy, name='refund_policy'),
 ]
