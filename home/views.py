bb37683ee7a87f6ba95f8d2588bfb956ff6129a8
JackTubby <jack200034@hotmail.com>
2021-06-24 19:01:45 +0000

    Remove faq view

--- home/views.py
+++ home/views.py
@@ -6,10 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-
-# Faq Page
-def faq(request):
-    """ A view to return the faq page """
-
-    return render(request, 'home/faq.html')
