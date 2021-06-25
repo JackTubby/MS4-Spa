82997024495692b351a73b71e8411ba7ed195e9e
JackTubby <jack200034@hotmail.com>
2021-06-25 09:46:48 +0000

    Update treatment description to have a max_length

--- treatments/models.py
+++ treatments/models.py
@@ -22,7 +22,7 @@ class Treatment(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.TextField(max_length=200)
     amount = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
