cd1df74b65ab6782247d228ab57b60df24df8dc9
JackTubby <jack200034@hotmail.com>
2021-06-25 10:18:31 +0000

    Add form validation for treatment

--- treatments/models.py
+++ treatments/models.py
@@ -20,10 +20,10 @@ class Category(models.Model):
 
 class Treatment(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=12)
+    name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
-    amount = models.IntegerField(default=0)
+    amount = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
